047f89a33e60fa248fd472fedd4945e8180561ad
peetbo <pdesignbg@gmail.com>
2019-09-13 09:35:20 +0300

    new ASP project

---
+++ 46093z/46093z/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_46093z.Global" Language="C#" %>
---
+++ 46093z/46093z/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_46093z.Startup))]
+namespace _46093z
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
